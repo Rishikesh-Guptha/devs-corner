c1d28bf3fe3045d2bb6662f97f4777682c25f1ff
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-15 23:05:50 +0530

    Initiated the landing,login.signup pages

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Landingpage from "./pages/landingpage/Landingpage";
+function App() {
+  return (
+    <div className="App">
+      <Landingpage/>
+    </div>
+  );
+}
+
+export default App;
