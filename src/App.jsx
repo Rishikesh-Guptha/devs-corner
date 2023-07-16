5487971ef409a969e0fe99b54c7188473910e116
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-16 10:05:35 +0530

    Added Navbar1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Landingpage from "./pages/landingpage/Landingpage";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar1 from "./pages/navbar1/Navbar1";
 function App() {
   return (
     <div className="App">
--- src/pages/landingpage/Landingpage.jsx
+++ src/pages/landingpage/Landingpage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Navbar1 from '../navbar1/Navbar1'
+import './/Landingpage.css'
 
 const Landingpage = () => {
   return (
     <div>
-        <div className="navbar">Navbar </div>
+        <div className="navbar"> 
+        <Navbar1 />
+        </div>
         <div className="main-text">
-            <h1 className="heading">Landing Page</h1>
+            <h1 className="heading">Dev's Corner</h1>
         </div>
     </div>
   )
