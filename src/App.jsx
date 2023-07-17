4ada448d62025f81037702c71f54296b47b865ec
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-17 22:10:30 +0530

    Used react Router and linked the login, signup and the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Landingpage from "./pages/landingpage/Landingpage";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Landingpage/> 
+    <div className="App"> 
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landingpage />}>
+        {/* <Route index element={<Landingpage />}/> */}
+        <Route path="Login" element={<Login/>} />
+        <Route path="Signup" element={<Signup/>} />
+       </Route> 
+      </Routes>
+      </BrowserRouter>
       
     </div>
   );
--- src/pages/landingpage/Landingpage.jsx
+++ src/pages/landingpage/Landingpage.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import './/Landingpage.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import Login from '../login/Login';
+import Signup from '../signup/Signup';
 
 const Landingpage = () => {
   return (
     <div>
         <div className="navbar"> 
         <div className="nav-left">
-        <p className="title">Dev's Corner</p>
+        <p className="title"> <Link to="/" style={{textDecoration: 'none', color: 'black'}}>Dev's Corner</Link></p>
         </div>
-
+        
         <div className="nav-right">
-          <p className="login">Login</p>
-          <p className="signup">Signup</p>
+          <p className="login"> <Link to="Login" style={{textDecoration: 'none', color: 'black'}}>Login</Link> </p>
+          <p className="signup"><Link to="Signup" style={{textDecoration: 'none', color: 'black'}}>Signup</Link></p>
         </div>
         
         </div>
         <div className="main-text">
             
         </div>
+        <Outlet/>
     </div>
   )
 }
