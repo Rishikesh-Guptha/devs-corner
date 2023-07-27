6c6ea0acb6e88bf57f40cf164e70be5dc38e6474
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-27 21:23:59 +0530

    Modified some code

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Dashboard.css';
 import Navbar2 from '../navbar2/Navbar2';
 import Login from '../login/Login';
-// import Dsa from '../dsa/Dsa.jsx';
+import Dsa from '../dsa/Dsa.jsx';
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar2/>
-      
+      <Dsa/>
       <div className="content">
         <p className='abc'>This is the page where the users get redirected after authentication</p>
         <p>This is the place where we display all the dsa, development etc...</p>
