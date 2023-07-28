5ef741da34d727d4dac8f623a40907ecd5cd6160
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-28 20:32:34 +0530

    Modified the dashboard components

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -13,10 +13,11 @@ const Dashboard = () => {
       <div className="main_content">
        <Box sx={{display: 'flex','& > :not(style)': {m: 1,width: 275,height: 150,},}}>
       {/* <Paper variant="outlined" /> */}
-      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray'}} />
-      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray'}} />
-      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray'}} />
-      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray'}} />
+      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray', borderRadius:5}} >Hello</Paper>
+      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray', borderRadius:5}} >Hello</Paper>
+      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray', borderRadius:5}} >Hello</Paper>
+      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'gray', borderRadius:5}} >Hello</Paper>
+      
     </Box>
     
 
