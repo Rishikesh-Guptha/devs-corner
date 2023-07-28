1c9052d9800e6cdfbb7986f9159f69572e2a0418
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-28 21:52:00 +0530

    Modified the dashboard components

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -14,9 +14,11 @@ const Dashboard = () => {
         <p className="headings">
           Courses
         </p>
-       <Box sx={{display: 'flex','& > :not(style)': {m: 1,width: 1200,height: 220,},}}>
+       <Box sx={{display: 'flex','& > :not(style)': {m: 5,width: 1400,height: 280,},}}>
       {/* <Paper variant="outlined" /> */}
-      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'#bdbdbd', borderRadius:5, i:100}} >Hello</Paper>
+      <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'#bdbdbd', borderRadius:5, i:100}} >
+      <Paper variant="outlined" square sx={{backgroundColor:'white',width:375, height: 240 ,m:2.5 }}>DSA</Paper>
+      </Paper>
       
     </Box>
     
