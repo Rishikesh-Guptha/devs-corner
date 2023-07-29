303b9c4863d8b1c57fa442b619f1a27a8731e29d
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-29 08:36:49 +0530

    modified the dashboard UI and installed mui-image

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,6 +5,8 @@ import Login from '../login/Login';
 import Dsa from '../dsa/Dsa.jsx';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+// or
+import { Image } from 'mui-image'
 
 const Dashboard = () => {
   return (
@@ -18,7 +20,9 @@ const Dashboard = () => {
       {/* <Paper variant="outlined" /> */}
       
       <Paper variant="outlined" square elevation={20} sx={{backgroundColor:'#bdbdbd', borderRadius:5, i:100, display:'flex'}} >
-      <Paper variant="outlined" square sx={{backgroundColor:'white',width:375, height: 240 ,m:2.5, borderRadius:5 }}></Paper>
+      {/* <Paper variant="outlined" component={'image'} alt="DSA pic" src='../assets/data_structure.jpg' square sx={{backgroundColor:'white',width:375, height: 240 ,m:2.5, borderRadius:5 }}></Paper> */}
+      <img src='../../assets/data_structure.jpg'alt='dsa pic' width={375} height={240}/>
+      {/* <Image src="../../assets/data_structure.jpg" alt='dsa pic' width={375} height={240}   /> */}
       <Paper variant="outlined" square sx={{alignItems:'stretch', backgroundColor:'#bdbdbd', height:260, width:1000, m:1, border:'none'}} >DSA</Paper>
       </Paper>
 
