714b8c8492d8ac17c9f8a543045a36ad9b0d120a
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-02 22:41:44 +0530

    Added contents in DSA page

--- src/pages/dsa/Dsa.jsx
+++ src/pages/dsa/Dsa.jsx
@@ -5,9 +5,18 @@ import Navbar2 from '../navbar2/Navbar2'
 const Dsa = () => {
   return (
     <div>
-        <div className="container">
+        <div className="main-container">
           <Navbar2/>
-          dsa
+          <div className="main-content">
+            <div className="text-content">
+            <p className="title">
+              Data Structure and Algorithms
+            </p>
+            <p className="title-text">
+              This is the Briyani of Non-vegetarian Foods, its inevitable...
+            </p>
+            </div>
+          </div>
         </div>
     </div>
   )
