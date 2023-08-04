6b72232aeedd8151d5988666889ec6a658de9552
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-04 23:58:03 +0530

    Modified the Opensource page

--- src/pages/opensource/Opensource.jsx
+++ src/pages/opensource/Opensource.jsx
@@ -14,7 +14,23 @@ const Opensource = () => {
           <p className="text-title">Open Source</p>
           <p className="title-text">This is the Space stone that opens a portal to great jobs for developers...</p>
         </div>
-        <div className="tech-content"></div>
+        <div className="tech-content">
+          <div className="tech-content-opensource">
+            <p className="tech-content-opensource-title">Basics</p>
+
+            <Accordion sx={{maxWidth:0.95, marginLeft:5,marginBottom:2}}>
+                  {/* <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" > */}
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content"   >
+                    <Typography>Git and Github</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ marginLeft:5}}>
+                    <Typography sx={{ marginLeft:5}}>
+                      The contents for how to use git and github lie here..
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+          </div>
+        </div>
       </div>
     </div>
   )
