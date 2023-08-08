6d2daa8ad8118c082acffe2570ca2e039f96c6fb
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-08 23:37:32 +0530

    Configured the Finance page UI

--- src/pages/finace/Finance.jsx
+++ src/pages/finace/Finance.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar2 from '../navbar2/Navbar2'
+import './Finance.css'
 
 const Finance = () => {
   return (
-    <div>Finance</div>
+    <div>
+      <Navbar2/>
+      <div className="main-content">
+        <div className="head">
+          <p className="title-text">Finance Managment</p>
+          <p className="text-title">
+            This is the place where you learn , earning is only a part of the process...
+          </p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
