b3df1d782c4a229eef195368fd4c44daa62dcbdd
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-11 23:12:08 +0530

    Added the drag and drop in array page

--- src/pages/data_structures/arrays/Array.jsx
+++ src/pages/data_structures/arrays/Array.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const Array = () => {
+  
+  const [droppedItems, setDroppedItems] = useState([]);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+
+    const data = e.dataTransfer.getData('text/plain');
+    const newItem = { type: 'link', content: data };
+
+    setDroppedItems([...droppedItems, newItem]);
+  };
   return (
-    <div>Array</div>
+    <div className='main-container'>
+      <DragDropContext onDragEnd={result => console.log(result)}>
+      <div className="drag-drop-area" onDragOver={handleDragOver} onDrop={handleDrop}>
+      <h2>Drag and Drop Area</h2>
+      <div className="dropped-items-container">
+        {droppedItems.map((item, index) => (
+          <div key={index} className="dropped-item">
+            {item.type === 'link' ? (
+              <a href={item.content} target="_blank" rel="noopener noreferrer">
+                {item.content}
+              </a>
+            ) : (
+              <div className="other-content">{item.content}</div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+      </DragDropContext>
+      
+    </div>
   )
 }
+
 
 export default Array
