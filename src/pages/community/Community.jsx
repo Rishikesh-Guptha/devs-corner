8973740a24b0f36822c9d6807c8b624b85caff15
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-13 14:18:29 +0530

    Configured the community page

--- src/pages/community/Community.jsx
+++ src/pages/community/Community.jsx
@@ -1,15 +1,68 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './Community.css';
 import Navbar2 from '../navbar2/Navbar2';
 import { Link } from 'react-router-dom';
-
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { db,auth } from '../../firebase';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 const Community = () => {
+
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, [deletePost]);
+
   return (
     <div>
         <Navbar2/>
         <div className="main-content">
-            <p>Community</p>
-            <Link to="/Create_posts"><p className="create-posts">Create post</p></Link>
+          <div className="header">
+          <p className='header-title'>Community</p>
+          <Link to="/Create_posts" style={{textDecoration: 'none', color: 'black'}}><button className="create-posts">Create post</button></Link>
+          </div>
+          <div className="content-text">
+          <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <p> {post.title}</p>
+              </div>
+              <div className="deletePost">
+                {post.author.id === auth.currentUser.uid && (
+                  <button className='delete-btn'
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  ><DeleteOutlinedIcon />
+                    
+                  </button>
+                  
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer"> {post.textcontent} </div>
+            <h3>@{post.author.email}</h3>
+          </div>
+        );
+      })}
+    </div>
+            
+            
+          </div>
             
         </div>
     </div>
