b1c68a9f62405046d6773c98528d925c74e5ce13
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-10-29 13:37:30 +0530

    Modified the responsiveness

--- src/pages/finace/Finance.jsx
+++ src/pages/finace/Finance.jsx
@@ -10,46 +10,51 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Finance = () => {
   return (
-    <div>
-      <Navbar2/>
-      <div className="main-content">
-        <div className="head">
-          <p className="title-text">Finance Managment</p>
-          <p className="text-title">
-            This is the place where you learn , earning is only a part of the process...
-          </p>
-        </div>
-        <div className="finance-content">
-          <p className="finance-content-title">Investments</p>
+		<div className="finance-content">
+			<Navbar2 />
+			<div className="main-content-finance">
+				<div className="head">
+					<p className="title-text">Finance Managment</p>
+					<p className="text-title">
+						This is the place where you learn , earning is only a part of the
+						process...
+					</p>
+				</div>
+				<div className="finance-content">
+					<p className="finance-content-title">Investments</p>
 
-          <Accordion sx={{maxWidth:0.95, marginLeft:5,marginBottom:2}}>
-            {/* <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" > */}
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content"   >
-              <Typography>Why Invest?</Typography>
-            </AccordionSummary>
-            <AccordionDetails sx={{ marginLeft:5}}>
-                <Typography sx={{ marginLeft:5}}>
-                      The contents for why to invest lie here..
-                </Typography>  
-            </AccordionDetails>
-          </Accordion>
+					<Accordion sx={{ maxWidth: 0.95, marginLeft: 5, marginBottom: 2 }}>
+						{/* <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" > */}
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls="panel1a-content">
+							<Typography>Why Invest?</Typography>
+						</AccordionSummary>
+						<AccordionDetails sx={{ marginLeft: 5 }}>
+							<Typography sx={{ marginLeft: 5 }}>
+								The contents for why to invest lie here..
+							</Typography>
+						</AccordionDetails>
+					</Accordion>
 
-          <p className="finace-content-title">Latest News</p>
-          <Accordion sx={{maxWidth:0.95, marginLeft:5,marginBottom:2}}>
-            {/* <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" > */}
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content"   >
-              <Typography>Finance News</Typography>
-            </AccordionSummary>
-            <AccordionDetails sx={{ marginLeft:5}}>
-                <Typography sx={{ marginLeft:5}}>
-                      <Link to="/Stocks">Stocks</Link>
-                </Typography>  
-            </AccordionDetails>
-          </Accordion>
-        </div>
-      </div>
-    </div>
-  )
+					<p className="finance-content-title">Latest News</p>
+					<Accordion sx={{ maxWidth: 0.95, marginLeft: 5, marginBottom: 2 }}>
+						{/* <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" > */}
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls="panel1a-content">
+							<Typography>Finance News</Typography>
+						</AccordionSummary>
+						<AccordionDetails sx={{ marginLeft: 5 }}>
+							<Typography sx={{ marginLeft: 5 }}>
+								<Link to="/Stocks">Stocks</Link>
+							</Typography>
+						</AccordionDetails>
+					</Accordion>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Finance
--- src/pages/jobs/Jobs.jsx
+++ src/pages/jobs/Jobs.jsx
@@ -5,30 +5,36 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import "./Jobs.css";
 
 const Jobs = () => {
-  return (
-    <div>
-        <Navbar2/>
-        <div className="main-container">
-            <div className="texts">
-              <p className="titles" style={{color:'white',marginTop:'2rem',marginLeft:'1rem'}}>Jobs and Oppurtunities</p>
-            </div>
-
-        </div>
-        <div className="jobs">
-        <Accordion
-									sx={{ maxWidth: 0.95, marginLeft: 5, marginBottom: 2 ,marginTop: 10}}>
-									<AccordionSummary
-										
-										aria-controls="panel1a-content">
-										<Typography>Jobs</Typography>
-									</AccordionSummary>
-									
-								</Accordion>
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="jobs-container">
+			<Navbar2 />
+			<div className="main-container-jobs">
+				<div className="texts">
+					<p
+						className="titles"
+						style={{ color: "white", marginTop: "2rem", marginLeft: "1rem" }}>
+						Jobs and Oppurtunities
+					</p>
+				</div>
+			</div>
+			<div className="jobs">
+				<Accordion
+					sx={{
+						maxWidth: 0.95,
+						marginLeft: 5,
+						marginBottom: 2,
+						marginTop: 10,
+					}}>
+					<AccordionSummary aria-controls="panel1a-content">
+						<Typography>Jobs</Typography>
+					</AccordionSummary>
+				</Accordion>
+			</div>
+		</div>
+	);
+};
 
 export default Jobs
